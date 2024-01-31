02013ee3c0b36d854b4c5d347ffbc5959552bd0b
syedafa <afwaan3@gmail.com>
2024-01-31 12:52:32 +0530

    header component added

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import SignIn from "./Pages/SignIn";
 import SignUp from "./Pages/SignUp";
 import About from "./Pages/About";
 import Profile from "./Pages/Profile";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/sign-in" element={<SignIn />} />
