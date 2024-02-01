413b68b351140ce0bb9bdd6808ee41476f082b2f
syedafa <afwaan3@gmail.com>
2024-02-01 16:02:55 +0530

    added sign up page

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,5 +1,29 @@
 function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="User Name"
+          className="border p-3 rounded-lg"
+          id="username"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          className="border p-3 rounded-lg"
+          id="email"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="border p-3 rounded-lg"
+          id="password"
+        />
+      </form>
+    </div>
+  );
 }
 
 export default SignUp;
