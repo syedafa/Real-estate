8558fd0f59d6dfd9b81f7803469391c3f5b55c28
syedafa <afwaan3@gmail.com>
2024-03-01 10:32:02 +0530

    added about page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,5 +1,68 @@
 function About() {
-  return <div>About</div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Afan Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, omnis
+        mollitia voluptate voluptatem non quibusdam quasi magni voluptatum autem
+        unde eligendi labore deleniti similique quae a accusamus adipisci
+        temporibus fugiat qui? Velit, consequuntur temporibus quam aspernatur,
+        tempore non rem molestiae doloribus omnis voluptate ut sed
+        exercitationem sequi possimus laboriosam esse, aperiam atque impedit!
+        Inventore odit recusandae voluptates nihil ex nisi perspiciatis officia
+        consequatur, possimus excepturi consequuntur aliquam eum cum voluptatum
+        at! Accusamus accusantium asperiores enim fugit sunt, aperiam magnam
+        mollitia aliquid temporibus. Corporis odit officiis aliquid consequatur,
+        optio nostrum, quibusdam dolor eveniet cupiditate, a nulla voluptatum
+        tempora qui neque quod perspiciatis unde repellendus molestiae. Porro,
+        natus. Quae inventore eius a, suscipit facilis minima, eos sunt
+        repudiandae modi, rem quas incidunt dolorem voluptatibus aperiam tempora
+        maxime nobis eaque odit rerum deleniti delectus molestiae totam hic.
+        Excepturi veniam illum quaerat perferendis voluptatibus optio
+        necessitatibus quae fuga tempora placeat eius quam quasi qui rerum,
+        reprehenderit similique blanditiis, labore ex error. Laborum aliquam
+        reprehenderit tempora nisi vero commodi, qui et veniam voluptate odit
+        enim nulla facere cumque quidem, itaque velit eos, ipsum amet alias in.
+        Repellendus explicabo a nulla doloremque, asperiores vero mollitia.
+        Nihil, possimus aliquam eius a nisi sapiente sint vitae cumque itaque
+        excepturi? Hic inventore similique dolorum, dolorem veritatis
+        dignissimos quos suscipit doloribus perspiciatis temporibus eum qui
+        voluptates officia voluptatem ad facilis eveniet dolor repellat
+        cupiditate. Architecto cum cumque tempore repellendus totam porro non,
+        provident cupiditate aperiam.{" "}
+      </p>
+      <p className="mb-4 text-slate-700">
+        animi eligendi illum commodi consectetur dicta. Assumenda laboriosam
+        porro magni tempore consequatur corporis eveniet quaerat voluptatum sed
+        tenetur dolor error nisi esse, tempora nemo soluta ea! Quasi est ad
+        excepturi cum quae autem doloremque ipsum, adipisci, dolore,
+        consequuntur inventore? Saepe similique dolore, placeat maiores incidunt
+        quos ratione enim velit sint voluptate mollitia sequi quod obcaecati
+        consectetur cupiditate fuga iste blanditiis, aut assumenda ex voluptas
+        vero corrupti doloremque. Atque facilis architecto rerum exercitationem
+        explicabo ipsam quam iste earum voluptatem tenetur soluta commodi
+        dolorem totam qui corporis hic, modi eaque voluptate vitae quasi.
+      </p>
+      <p className="mb-4 text-slate-700">
+        consectetur dolore porro aliquid. Doloremque, neque possimus cupiditate
+        aspernatur, eaque voluptas beatae nisi ratione soluta sapiente, sit
+        labore blanditiis explicabo fugit voluptate sunt modi ipsam consectetur
+        assumenda ad nulla expedita pariatur natus? Tenetur laudantium totam,
+        iure provident dolor amet, eum id asperiores vitae voluptatem ad
+        sapiente veritatis animi inventore repudiandae neque libero vel
+        repellendus ut voluptatibus. Voluptatibus, a. Amet dolor laudantium
+        numquam quo deleniti nostrum dolorem, recusandae reprehenderit illo
+        ipsam perferendis itaque pariatur! Maiores eius necessitatibus, illo
+        corporis similique tenetur sint ratione nisi inventore autem omnis ut,
+        excepturi itaque fuga dolorum. Quia, quaerat corrupti, ea animi unde
+        expedita architecto officiis mollitia quasi commodi dolore error vero
+        aperiam optio maiores soluta illum neque est dignissimos nemo quas
+        nesciunt incidunt impedit laudantium! Ad voluptatum corporis at
+      </p>
+    </div>
+  );
 }
 
 export default About;
